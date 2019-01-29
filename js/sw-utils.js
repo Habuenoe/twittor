1f48539777b33c01c821d6cbae75399fca50dfc4
Hector Bueno <hectorbuenoenero@gmail.com>
2019-01-29 15:50:07 +0100

    Primer Comit

---
+++ js/sw-utils.js
@@ -0,0 +1,19 @@
+
+// Guardar en el caché dinámico
+function actualizaCacheDinamico(dynamicCache, req, res) {
+
+    if (res.ok) {
+
+        caches.open(dynamicCache).then(cache => {
+
+            cache.put(req, res.clone());
+
+            return res.clone();
+        });
+
+    } else {
+
+        return res;
+    }
+
+}
